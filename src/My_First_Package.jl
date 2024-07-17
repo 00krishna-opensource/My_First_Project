beb1e84970d7b0747d8dfc9cffb2d2e8217504f6
00krishna <cyclotomiq@gmail.com>
2024-07-16 19:50:32 -0700

    Committing my_second_function.

--- src/My_First_Package.jl
+++ src/My_First_Package.jl
@@ -6,6 +6,12 @@ function my_first_function()
     print("Hello from the package")
 end
 
-export my_first_function
+function my_second_function()
+    return 1 + 7
+end 
+
+
+
+export my_first_function, my_second_function
 
 end
